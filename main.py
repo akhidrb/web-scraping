22576232f8d98a097a2bd8018195bf99b67435e0
ibrahimkhidr <akhidrb@gmail.com>
2024-02-27 14:58:48 +0200

    scrape imdb

--- main.py
+++ main.py
@@ -1,1 +1,33 @@
-print('Hello Man')
+import requests
+
+from bs4 import BeautifulSoup
+import pandas as pd
+import time
+
+# URL of the website to scrape
+url = "https://www.imdb.com/chart/top"
+
+HEADERS = {'User-Agent': 'Mozilla/5.0 (iPad; CPU OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) '
+                         'Mobile/15E148'}
+
+# Send an HTTP GET request to the website
+response = requests.get(url, headers=HEADERS)
+print(response.headers)
+
+# Parse the HTML code using BeautifulSoup
+soup = BeautifulSoup(response.content, 'html.parser')
+
+# Extract the relevant information from the HTML code
+movies = []
+for row in soup.select('ul'):
+    title = row.find('td', class_='titleColumn').find('a').get_text()
+    year = row.find('td', class_='titleColumn').find('span', class_='secondaryInfo').get_text()[1:-1]
+    rating = row.find('td', class_='ratingColumn imdbRating').find('strong').get_text()
+    movies.append([title, year, rating])
+
+# Store the information in a pandas dataframe
+df = pd.DataFrame(movies, columns=['Title', 'Year', 'Rating'])
+
+# Add a delay between requests to avoid overwhelming the website with requests
+time.sleep(1)
+df.to_csv('top-rated-movies.csv', index=False)
